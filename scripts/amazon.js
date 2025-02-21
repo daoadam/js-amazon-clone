f2ec6cb37022e2da42bd035b1d2f71c12802adda
daoadam <adamdao.xd@gmail.com>
2025-02-21 13:25:08 +1030

    Used products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,11 @@
-console.log("nihao")
+
 
 //how to save the information?
 //create a variable that stores a list of objects
 //each object has a name, price, rating and image
 //it is stored in an array that can add(push) and remove(pop)
 
-const products = [{
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-        stars: 4.5,
-        score: 87,
-    }, //this has 2 parts, the stars and the number so we can make it into another object
-    priceCents: 1090
-}, {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating:{
-        stars: 4,
-        score: 127
-    },
-    priceCents: 2095,
-}, {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating:{
-        stars: 4.5,
-        score: 56
-    },
-    priceCents: 799,
-}]
+
 
 let productsGrid = document.querySelector(".js-products-grid");
 
